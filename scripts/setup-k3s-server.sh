ba4ebbc00ae0f76be8d88aab4ea7fb8e7c012ed9
blbergo <bryan.l.bergo@gmail.com>
2025-03-26 11:46:43 -0700

    feat: sync

--- scripts/setup-k3s-server.sh
+++ scripts/setup-k3s-server.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 set -e
 
+# Install kubectl
+echo "Installing kubectl..."
+
+
 echo "Installing k3s..."
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig=$HOME/local-cluster.config --write-kubeconfig-mode=644 --disable=traefik --cloud-provider=external -- --kubelet-arg='cloud-provider=external'" sh -
-export KUBECONFIG=$HOME/local-cluster.config
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=$FLOATING_IP --write-kubeconfig-mode 644" sh -
 
 echo "Waiting for k3s to be ready..."
 until kubectl get nodes &>/dev/null; do
+  kubectl get nodes
   sleep 2
 done
 echo "k3s is ready!"
 
-kubectl create secret -n kube-system generic cloud-config --from-file=/tmp/kustomize/base/openstack/cloud-config || true
+kubectl create secret -n kube-system generic cloud-config --from-file=/tmp/kustomize/base/openstack/cloud.conf || true
+#kubectl create secret -n kube-system generic cloud-config --from-file=cluster/base/openstack/cloud.conf || true
 
 kubectl apply -k /tmp/kustomize/base/openstack
 kubectl apply -k /tmp/kustomize/base/argocd
+#kubectl apply -k cluster/base/openstack
+#kubectl apply -k cluster/base/argocd
 
 echo "Waiting for ArgoCD server to be ready..."
 until kubectl get svc -n argocd-server &>/dev/null; do
   sleep 2
 done
 
-kubectl patch svc argocd-server -n argocd -p '{"metadata": {"annotations": {"service.beta.kubernetes.io/openstack-internal-load-balancer": "false"}}}'
-
 echo "Waiting for ArgoCD server to get an external IP..."
-until curl -s -o /dev/null -w "%{http_code}" http://$FLOATING_IP &>/dev/null; do
+until [[ -n "$FLOATING_IP" ]]; do
+  kubectl get svc argocd-server -n argocd -o jsonpath="{.status.loadBalancer.ingress[0].ip}"
+  FLOATING_IP=$(kubectl get svc argocd-server -n argocd -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
   sleep 2
 done
 
 echo "ArgoCD server is available at http://$FLOATING_IP"
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
-
-exit 1
--- terraform/modules/k3s/server/main.ts
+++ terraform/modules/k3s/server/main.ts
@@ -4,7 +4,6 @@ import { ComputeInstanceV2 } from "../../../.gen/providers/openstack/compute-ins
 import { NetworkingFloatingipV2 } from "../../../.gen/providers/openstack/networking-floatingip-v2";
 import { TerraformOutput } from "cdktf";
 import { NetworkingSecgroupV2 } from "../../../.gen/providers/openstack/networking-secgroup-v2";
-import { NetworkingSecgroupRuleV2 } from "../../../.gen/providers/openstack/networking-secgroup-rule-v2";
 import { NetworkingPortV2 } from "../../../.gen/providers/openstack/networking-port-v2";
 import { DataOpenstackNetworkingNetworkV2 } from "../../../.gen/providers/openstack/data-openstack-networking-network-v2";
 import { NetworkingFloatingipAssociateV2 } from "../../../.gen/providers/openstack/networking-floatingip-associate-v2";
@@ -22,75 +21,29 @@ export class K3sServerModule extends Construct {
     constructor(scope: Construct, id: string, props: k3sServerModuleProps) {
         super(scope, id);
 
-        const { ENVIRONMENT, DEFAULT_IMAGE_NAME, Security } = props;
+        const { DEFAULT_IMAGE_NAME, Security } = props;
         const k3sServer_FLAVOR_NAME = props.K3S_SERVER_FLAVOR_NAME || "m3.small";
 
-        const k3sServerKeyPair = new ComputeKeypairV2(this, `${ENVIRONMENT}-k3s-server-keypair`, {
-            name: `${ENVIRONMENT}-k3s-server-keypair`,
+        const k3sServerKeyPair = new ComputeKeypairV2(this, `k3s-server-keypair`, {
+            name: `k3s-server-keypair`,
         });
 
-        const k3sServerSecurityGroup = new NetworkingSecgroupV2(this, `${ENVIRONMENT}-k3s-server-security-group`, {
-            name: `${ENVIRONMENT}-k3s-server-security-group`,
+        const k3sServerSecurityGroup = new NetworkingSecgroupV2(this, `k3s-server-security-group`, {
+            name: `-k3s-server-security-group`,
             description: 'Allow PostgreSQL traffic',
         });
 
-        // ArgoCD API
-        new NetworkingSecgroupRuleV2(this, `${ENVIRONMENT}-k3s-server-security-ingress-rule-3`, {
-            direction: 'ingress',
-            ethertype: 'IPv4',
-            protocol: 'tcp',
-            portRangeMax: 80,
-            portRangeMin: 80,
-            securityGroupId: k3sServerSecurityGroup.id,
-        });
-
-        new NetworkingSecgroupRuleV2(this, `${ENVIRONMENT}-k3s-server-security-ingress-rule-4`, {
-            direction: 'ingress',
-            ethertype: 'IPv4',
-            protocol: 'tcp',
-            portRangeMax: 443,
-            portRangeMin: 443,
-            securityGroupId: k3sServerSecurityGroup.id,
-        });
-
-        new NetworkingSecgroupRuleV2(this, `${ENVIRONMENT}-k3s-server-security-ingress-rule-5`, {
-            direction: 'ingress',
-            ethertype: 'IPv4',
-            protocol: 'tcp',
-            portRangeMax: 6443,
-            portRangeMin: 6443,
-            securityGroupId: k3sServerSecurityGroup.id,
-        });
-
-        new NetworkingSecgroupRuleV2(this, `${ENVIRONMENT}-k3s-server-security-ingress-rule-6`, {
-            direction: 'ingress',
-            ethertype: 'IPv4',
-            protocol: 'tcp',
-            portRangeMax: 10250,
-            portRangeMin: 10250,
-            securityGroupId: k3sServerSecurityGroup.id,
-        });
-
-        new NetworkingSecgroupRuleV2(this, `${ENVIRONMENT}-k3s-server-security-ingress-rule-7`, {
-            direction: 'ingress',
-            ethertype: 'IPv4',
-            protocol: 'tcp',
-            portRangeMax: 8080,
-            portRangeMin: 8080,
-            securityGroupId: k3sServerSecurityGroup.id,
-        });
-
-        const defaultNet = new DataOpenstackNetworkingNetworkV2(this, `${ENVIRONMENT}-default-net`, {
+        const defaultNet = new DataOpenstackNetworkingNetworkV2(this, `default-net`, {
             name: "auto_allocated_network",
         });
 
-        const k3sServerPort = new NetworkingPortV2(this, `${ENVIRONMENT}-k3s-server-port`, {
+        const k3sServerPort = new NetworkingPortV2(this, `k3s-server-port`, {
             networkId: defaultNet.id,
             securityGroupIds: [k3sServerSecurityGroup.id, Security.getSshSecurityGroup().id],
         });
 
-        const k3sServerInstance = new ComputeInstanceV2(this, `${ENVIRONMENT}`, {
-            name: `${ENVIRONMENT}-k3s-server`,
+        const k3sServerInstance = new ComputeInstanceV2(this, `k3s-server`, {
+            name: `k3s-server`,
             imageName: DEFAULT_IMAGE_NAME,
             flavorName: k3sServer_FLAVOR_NAME,
             keyPair: k3sServerKeyPair.name,
@@ -98,19 +51,19 @@ export class K3sServerModule extends Construct {
             dependsOn: [k3sServerPort],
         });
 
-        const k3sServerInstanceIp = new NetworkingFloatingipV2(this, `${ENVIRONMENT}-k3s-server-ip`, {
+        const k3sServerInstanceIp = new NetworkingFloatingipV2(this, `k3s-server-ip`, {
             pool: "public",
             portId: k3sServerPort.id,
             dependsOn: [k3sServerPort],
         });
 
-        const k3sServerIpAssociate = new NetworkingFloatingipAssociateV2(this, `${ENVIRONMENT}-k3s-server-ip-associate`, {
+        const k3sServerIpAssociate = new NetworkingFloatingipAssociateV2(this, `k3s-server-ip-associate`, {
             floatingIp: k3sServerInstanceIp.address,
             portId: k3sServerPort.id,
             dependsOn: [k3sServerInstanceIp],
         });
 
-        new Resource(this, `${ENVIRONMENT}-k3s-server-instance-setup`, {
+        new Resource(this, `k3s-server-instance-setup`, {
             dependsOn: [k3sServerIpAssociate, k3sServerInstanceIp, k3sServerInstance],
             provisioners: [{
                 type: "file",
@@ -125,9 +78,8 @@ export class K3sServerModule extends Construct {
             {
                 type: "remote-exec",
                 inline: [
-                    `export FLOATING_IP=${k3sServerInstanceIp.address}`,
                     "chmod +x /tmp/setup-k3s-server.sh",
-                    "/tmp/setup-k3s-server.sh",
+                    "sudo /tmp/setup-k3s-server.sh",
                 ]
             }],
             connection: {
@@ -138,7 +90,7 @@ export class K3sServerModule extends Construct {
             }
         })
 
-        new TerraformOutput(this, `${ENVIRONMENT}-k3s-server-ip-output`, {
+        new TerraformOutput(this, `k3s-server-ip-output`, {
             value: k3sServerInstanceIp.address,
         });
     }
