8bbe82854d1e642f969e63da64562979930fd63f
blbergo <bryan.l.bergo@gmail.com>
2025-03-26 14:16:53 -0700

    feat: sync

--- scripts/setup-k3s-server.sh
+++ scripts/setup-k3s-server.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-FLOATING_IP=$(curl https://ipinfo.io/ip)
-
 echo "Installing k3s..."
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig=$HOME/local-cluster.config --write-kubeconfig-mode=644" sh -
 export KUBECONFIG=$HOME/local-cluster.config
@@ -12,14 +10,18 @@ until kubectl get nodes &>/dev/null; do
   kubectl get nodes
   sleep 2
 done
-echo "k3s is ready!"
 
-kubectl create secret -n kube-system generic cloud-config --from-file=/tmp/kustomize/base/openstack/cloud.conf || true
-#kubectl create secret -n kube-system generic cloud-config --from-file=cluster/base/openstack/cloud.conf || true
+echo "Waiting for Traefik to be assigned an external IP..."
+until kubectl get svc -n kube-system traefik &>/dev/null; do
+  sleep 2
+done
+
+#kubectl create secret -n kube-system generic cloud-config --from-file=/tmp/kustomize/base/openstack/cloud.conf || true
+kubectl create secret -n kube-system generic cloud-config --from-file=cluster/base/openstack/cloud.conf || true
 
 #kubectl apply -k /tmp/kustomize/base/openstack
 #kubectl apply -k /tmp/kustomize/base/argocd
-kubectl apply -k cluster/base/openstack
+#kubectl apply -k cluster/base/openstack
 kubectl apply -k cluster/base/argocd
 
 echo "Waiting for ArgoCD server to be ready..."
@@ -27,5 +29,10 @@ until kubectl get svc -n argocd-server &>/dev/null; do
   sleep 2
 done
 
-echo "ArgoCD server is available at http://$FLOATING_IP"
+sleep 30
+
+kubectl apply -f cluster/base/argocd/ingress.yaml
+
+IP=$(kubectl get svc -n kube-system traefik -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+echo "ArgoCD server is available at http://$IP"
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
