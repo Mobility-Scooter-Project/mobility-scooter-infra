34f499c317f99ac4f17ce483d859e5698b5b27b9
blbergo <bryan.l.bergo@gmail.com>
2025-03-28 11:29:49 -0700

    feat: argocd dashboard

--- scripts/setup-k3s-server.sh
+++ scripts/setup-k3s-server.sh
@@ -10,7 +10,7 @@ until kubectl get nodes &>/dev/null; do
   sleep 2
 done
 
-kubectl patch node "$(hostname)" -p '{"spec":{"providerID":"openstack://IU/'${INSTANCE_ID}'"}}'
+kubectl patch node "$(kubectl get nodes -o jsonpath='{.items[0].metadata.name}')" -p '{"spec":{"providerID":"openstack://IU/'${INSTANCE_ID}'"}}'
 
 kubectl create secret -n kube-system generic cloud-config --from-file=/tmp/cluster/base/openstack/cloud.conf || true
 kubectl apply -k /tmp/cluster/base/openstack
@@ -22,8 +22,13 @@ done
 
 kubectl apply -k /tmp/cluster/base/traefik
 
-#helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+# Install helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+# Install ArgoCD
 kubectl apply -k /tmp/cluster/base/argocd
 
 echo "Waiting for ArgoCD server to be ready..."
@@ -31,9 +36,11 @@ until kubectl get svc -n argocd-server &>/dev/null; do
   sleep 2
 done
 
-#kubectl apply -f /tmp/cluster/base/argocd/ingress.yaml
+kubectl apply -f /tmp/cluster/base/argocd/ingress.yaml
 
 IP=$(kubectl get svc -n kube-system traefik -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
 echo "Traefik Dashboard is available at http://$IP/dashboard/"
 echo "ArgoCD server is available at https://$IP/argocd"
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
+
+# TODO: assign dns records
