097d46723f7c70141dba6cea909ab236efd0c9f1
Bryan <bryan.l.bergo@gmail.com>
2025-08-03 17:19:17 -0700

    feat: redpanda connection

--- scripts/port_forward.sh
+++ scripts/port_forward.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
 kubectl port-forward svc/infisical-backend -n infisical 8081:8080 > /dev/null 2>&1 &
+
+echo "ArgoCD is now accessible at https://localhost:8080"
+echo "Infisical is now accessible at http://localhost:8081"
