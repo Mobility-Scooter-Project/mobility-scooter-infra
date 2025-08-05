71e5eb09b8ff1d4f66ee135e0745c2100707780a
Bryan <bryan.l.bergo@gmail.com>
2025-08-04 23:32:16 -0700

    feat: valkey setup

--- scripts/create_k3d_cluster.sh
+++ scripts/create_k3d_cluster.sh
@@ -5,5 +5,5 @@ kubectl apply -k cluster/bootstrap
 echo "Waiting for ArgoCD to be ready..."
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
 echo "ArgoCD is ready. Installing helm charts..."
-kubectl apply -k cluster/helm
+kubectl apply -k cluster/helm --server-side
 echo "Cluster bootstrapped successfully."
