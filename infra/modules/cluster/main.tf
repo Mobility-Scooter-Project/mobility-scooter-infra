dc6c6a76161950c37c073be5fb606d178f2299f4
Bryan <bryan.l.bergo@gmail.com>
2025-08-27 23:47:41 -0700

    fix: remove extra end in worker

--- infra/modules/cluster/main.tf
+++ infra/modules/cluster/main.tf
@@ -30,7 +30,9 @@ resource "openstack_containerinfra_nodegroup_v1" "gpu_nodegroup" {
   node_count = 1
   flavor_id     = "g3.medium"
   image_id   = "74846576-bb7e-4ca9-897e-8f33e8fd84d1"
+  # NOTE: this did not actually end up working, so I manually set the volume size in the OpenStack dashboard to 100GB
+  docker_volume_size = 100
   labels    = {
-    boot_volume_size = "50"
+    boot_volume_size = "60"
   }
 }
