c0627ce8978afaf11732b620b841aac0da34d0ae
Bryan <bryan.l.bergo@gmail.com>
2025-09-09 14:05:23 -0700

    fix: update env script

--- scripts/pull_env.sh
+++ scripts/pull_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-infisical login --domain https://infisical.cis240470.projects.jetstream-cloud.org
-infisical secrets --projectId 3204844a-4f4c-479e-b776-1d70588f696c --env prod --plain --tags local > .env
+infisical login --domain http://infisical.cis240470.projects.jetstream-cloud.org
+infisical secrets --projectId 6b6bec38-9035-4fdd-a8c3-376af197b0c0 --env prod --plain --tags local | grep -v ".env=" > .env
 
 echo "Pulled latest .env from Infisical"
