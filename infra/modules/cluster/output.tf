6915edfd7d40b46b92ad030601cdbac049221f58
Bryan <bryan.l.bergo@gmail.com>
2025-09-15 03:00:20 -0700

    wip

--- infra/modules/cluster/output.tf
+++ infra/modules/cluster/output.tf
@@ -1,4 +1,10 @@
 output "kubeconfig" {
-  value = openstack_containerinfra_cluster_v1.msp_cluster_prod.kubeconfig.raw_config
+  value     = openstack_containerinfra_cluster_v1.msp_cluster_prod.kubeconfig.raw_config
   sensitive = true
+}
+
+output "cluster_keypair" {
+  description = "The cluster keypair"
+  value       = openstack_compute_keypair_v2.msp_keypair.private_key
+  sensitive   = true
 }
